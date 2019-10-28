41e8a6e6e1e0ab4094fad11050d21eea335ecc3a
Zolton <gmzesq@gmail.com>
2019-10-28 16:19:02 -0400

    2/3 done

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,13 +11,18 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
-
+for gabba in range(5):
+    y.append(gabba + 1)
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
+
+
 y = []
+for gabba in range(10):
+    y.append(gabba**3)
 
 print(y)
 
@@ -27,6 +32,8 @@ print(y)
 a = ["foo", "bar", "baz"]
 
 y = []
+for gabba in a:
+    y.append(gabba.upper())
 
 print(y)
 
@@ -37,5 +44,8 @@ x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
 y = []
+
+if gabba in x % 2 == 0:
+    y.append(gabba)
 
 print(y)
