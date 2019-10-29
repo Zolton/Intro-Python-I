411dad02a026423251f0690103315394ad97c5ee
Zolton <gmzesq@gmail.com>
2019-10-29 16:45:58 -0400

    Done

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -23,12 +23,6 @@ def read_only(x):
     return readFile
 print(read_only("src/foo.txt"))
 
-
-
-
-
-
-
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
